5d77631c5a3cd41509ce3784a6f2bb3ce4d99e0c
unknown <allisoncarranza16@gmail.com>
2021-02-15 20:05:45 -0600

    [ADD] Herencia de vistas y modulos.

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/openacademy.xml',
+        'views/partner.xml',
         #'views/course.xml',
         'demo/demo.xml',
     ],
